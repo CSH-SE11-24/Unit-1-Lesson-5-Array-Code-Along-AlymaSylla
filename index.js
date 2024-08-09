0c1be7ae374a2a10a95c7a86c2bebdeaa3296f7c
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-09 12:56:34 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// Declare a variable that stores an array of any 3 fictional people
+let fictional=["spiderman","flash","robin"]
+// Console log the first element of the array (using array name and index)
+console.log(fictional[0])
+// Console log the last element of the array (using array name and index)
+console.log(fictional[2])
+// Console log the last element of the array WITHOUT using the index -- use the array length instead
+console.log(fictional.length)
+console.log(fictional[fictional.length-1])
+// Change the second element of the array to be a different fictional character
+
+// Stretch: try ADDING a new fictional character to the array
+
+let num1 = prompt(`Enter a number`)
+let num2 = prompt(`Enter another number`)
+Number(num1)
+Number(num2)
+let sum = num1 + num2
+console.log(sum)
+
+
